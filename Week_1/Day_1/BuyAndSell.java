be59cd4b24545bb20fc999ad0356d345cda398aa
yadav-anuragx07 <annurag1221@gmail.com>
2026-06-01 21:55:59 +0530

    Week 1 Day 1: Best Time to Buy and Sell Stock solution

---
+++ Week_1/Day_1/BuyAndSell.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int buy = prices[0];
+        int maxProfit = 0;
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] < buy) {
+                buy = prices[i];
+            }
+            if (prices[i] - buy > maxProfit) {
+                maxProfit = prices[i] - buy;
+            }
+        }
+        return maxProfit;
+    }
+}
