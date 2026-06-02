73899ef4a87e686b6def6f2763763c4476280405
yadav-anuragx07 <annurag1221@gmail.com>
2026-06-02 21:26:57 +0530

    Week 1 Day 2: ContainsDuplicate solution

---
+++ Week_1/Day_2/ContainsDuplicate.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int i=0; i<nums.length; i++){
+            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
+            if(map.get(nums[i])>1){
+                return true;
+            }
+        }
+        return false;
+    }
+}
